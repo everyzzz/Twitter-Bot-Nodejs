ced140b1919a7f53f5ed8598a52c2771f49e758f
everyzzz <jquisper2@gmail.com>
2023-01-26 13:45:07 -0500

    "add_twitt_function"

---
+++ netlify/functions/bot.ts
@@ -0,0 +1,39 @@
+import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+import  { config } from "dotenv";
+import { TwitterApi } from "twitter-api-v2";
+
+config();
+
+const appKey: any = process.env.TWITTER_CUSTOMER_KEY
+const appSecret: any = process.env.TWITTER_CUSTOMER_SECRET
+const accessToken: any = process.env.TWITTER_ACCESS_TOKEN
+const accessSecret: any  = process.env.TWITTER_ACCESS_TOKEN_SECRET
+
+const twitterClient = new TwitterApi({
+  appKey: appKey,
+  appSecret: appSecret,
+  accessToken: accessToken,
+  accessSecret: accessSecret
+});
+
+const rwClient = twitterClient.readWrite;
+
+const main = async () => {
+  await rwClient.v2
+    .tweet("Hello World!")
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ ok: true, message: main() }),
+  };
+};
+
+export { handler };
