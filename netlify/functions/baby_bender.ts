bf47400764ba9674d9951bec347ced8ecf6fede9
everyzzz <jquisper2@gmail.com>
2023-01-26 16:58:27 -0500

    "add_baby_bender_function"

---
+++ netlify/functions/baby_bender.ts
@@ -0,0 +1,37 @@
+import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+import  { config } from "dotenv";
+import { TwitterApi } from "twitter-api-v2";
+
+config();
+
+const appKey: any = process.env.TWITTER_CUSTOMER_KEY
+const appSecret: any = process.env.TWITTER_CUSTOMER_SECRET
+const accessToken: any = process.env.ACCESS_TOKEN_BOT
+const accessSecret: any  = process.env.TOKEN_SECRET_BOT
+
+const twitterClient = new TwitterApi({
+  appKey: appKey,
+  appSecret: appSecret,
+  accessToken: accessToken,
+  accessSecret: accessSecret
+});
+
+const twitt = "Hello world!"
+
+const rwClient = twitterClient.readWrite;
+
+const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+  await rwClient.v2
+    .tweet(twitt)
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+  });
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ ok: true, message: twitt }),
+  };
+};
+export { handler };
