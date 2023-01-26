823778e8631a215c1e5f6305b8dab60c530b2850
everyzzz <jquisper2@gmail.com>
2023-01-26 14:55:08 -0500

    "test"

--- netlify/functions/bot.ts
+++ netlify/functions/bot.ts
@@ -17,21 +17,7 @@ const twitterClient = new TwitterApi({
 });
 
 const twitt = "Hello World!"
-
-
 const rwClient = twitterClient.readWrite;
-
-// const main = async () => {
-//   await rwClient.v2
-//     .tweet(twitt)
-//     .then((response) => {
-//       console.log(response);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     });
-// };
-
 
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
   await rwClient.v2
@@ -47,67 +33,4 @@ const handler: Handler = async (event: HandlerEvent, context: HandlerContext) =>
     body: JSON.stringify({ ok: true, message: twitt }),
   };
 };
-// main();, main 
 export { handler };
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
-// import  { config } from "dotenv";
-// import { TwitterApi } from "twitter-api-v2";
-
-// config();
-
-// const appKey: any = process.env.TWITTER_CUSTOMER_KEY
-// const appSecret: any = process.env.TWITTER_CUSTOMER_SECRET
-// const accessToken: any = process.env.TWITTER_ACCESS_TOKEN
-// const accessSecret: any  = process.env.TWITTER_ACCESS_TOKEN_SECRET
-
-// const twitterClient = new TwitterApi({
-//   appKey: appKey,
-//   appSecret: appSecret,
-//   accessToken: accessToken,
-//   accessSecret: accessSecret
-// });
-
-// const rwClient = twitterClient.readWrite;
-
-// const main = async () => {
-//   await rwClient.v2
-//     .tweet("Hello World!")
-//     .then((response) => {
-//       console.log(response);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     });
-// };
-
-
-// const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
-//   await rwClient.v2
-//     .tweet("Hello World!")
-//     .then((response) => {
-//       console.log(response);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//   });
-  
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify({ ok: true }),
-//   };
-// };
-// main();
-// export { handler, main };
