2359f7d46fcf1773049354754b879fe217f24e5b
everyzzz <jquisper2@gmail.com>
2023-01-26 16:35:02 -0500

    "test2"

--- netlify/functions/bot2.ts
+++ netlify/functions/bot2.ts
@@ -9,7 +9,8 @@ const appSecret: any = process.env.TWITTER_CUSTOMER_SECRET
 const accessToken: any = process.env.TWITTER_ACCESS_TOKEN
 const accessSecret: any  = process.env.TWITTER_ACCESS_TOKEN_SECRET
 
-const message = "Hola mundo!"
+const twitt = "Hola mundo!"
+
 const T = new Twit({
   consumer_key: appKey,
   consumer_secret: appSecret ,
@@ -17,14 +18,13 @@ const T = new Twit({
   access_token_secret: accessSecret
 });
 
-
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
-	await T.post('statuses/update', { status: message }, function (err, data, response) {
+	await T.post('statuses/update', { status: twitt }, function (err, data, response) {
 		console.log(data)
 	 });	
 	return {
 	  statusCode: 200,
-	  body: JSON.stringify({ ok: true, message: message }),
+	  body: JSON.stringify({ ok: true, message: twitt }),
 	};
  };
  export { handler };
