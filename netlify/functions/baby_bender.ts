3d9c1642b35b5895e1d9b6d42431fd69a4e92b1d
everyzzz <jquisper2@gmail.com>
2023-01-26 17:07:01 -0500

    "fix1_baby_bender_function"

--- netlify/functions/baby_bender.ts
+++ netlify/functions/baby_bender.ts
@@ -1,6 +1,6 @@
+import  Twit  from "twit"
+import { config } from "dotenv";
 import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
-import  { config } from "dotenv";
-import { TwitterApi } from "twitter-api-v2";
 
 config();
 
@@ -9,29 +9,22 @@ const appSecret: any = process.env.TWITTER_CUSTOMER_SECRET
 const accessToken: any = process.env.ACCESS_TOKEN_BOT
 const accessSecret: any  = process.env.TOKEN_SECRET_BOT
 
-const twitterClient = new TwitterApi({
-  appKey: appKey,
-  appSecret: appSecret,
-  accessToken: accessToken,
-  accessSecret: accessSecret
+const twitt = "Hello World!!"
+
+const T = new Twit({
+  consumer_key: appKey,
+  consumer_secret: appSecret ,
+  access_token: accessToken,
+  access_token_secret: accessSecret
 });
 
-const twitt = "Hello world!"
-
-const rwClient = twitterClient.readWrite;
-
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
-  await rwClient.v2
-    .tweet(twitt)
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((error) => {
-      console.log(error);
-  });
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ ok: true, message: twitt }),
-  };
-};
-export { handler };
+	await T.post('statuses/update', { status: twitt }, function (err, data, response) {
+		console.log(data)
+	 });	
+	return {
+	  statusCode: 200,
+	  body: JSON.stringify({ ok: true, message: twitt }),
+	};
+ };
+ export { handler };
