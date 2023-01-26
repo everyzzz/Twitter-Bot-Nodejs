110308d684be3fb34c90e4e3a93b336b1183b232
everyzzz <jquisper2@gmail.com>
2023-01-26 14:46:57 -0500

    "test"

--- netlify/functions/bot.ts
+++ netlify/functions/bot.ts
@@ -16,11 +16,13 @@ const twitterClient = new TwitterApi({
   accessSecret: accessSecret
 });
 
-const rwClient = twitterClient.readWrite;
+const twitt = "Hello World!"
 
+
+const rwClient = twitterClient.readWrite;
 const main = async () => {
   await rwClient.v2
-    .tweet("Hello World!")
+    .tweet(twitt)
     .then((response) => {
       console.log(response);
     })
@@ -31,19 +33,72 @@ const main = async () => {
 
 
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
-  await rwClient.v2
-    .tweet("Hello World!")
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((error) => {
-      console.log(error);
-  });
-  
   return {
     statusCode: 200,
-    body: JSON.stringify({ ok: true }),
+    body: JSON.stringify({ ok: true, message: twitt }),
   };
 };
 main();
 export { handler, main };
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+// import  { config } from "dotenv";
+// import { TwitterApi } from "twitter-api-v2";
+
+// config();
+
+// const appKey: any = process.env.TWITTER_CUSTOMER_KEY
+// const appSecret: any = process.env.TWITTER_CUSTOMER_SECRET
+// const accessToken: any = process.env.TWITTER_ACCESS_TOKEN
+// const accessSecret: any  = process.env.TWITTER_ACCESS_TOKEN_SECRET
+
+// const twitterClient = new TwitterApi({
+//   appKey: appKey,
+//   appSecret: appSecret,
+//   accessToken: accessToken,
+//   accessSecret: accessSecret
+// });
+
+// const rwClient = twitterClient.readWrite;
+
+// const main = async () => {
+//   await rwClient.v2
+//     .tweet("Hello World!")
+//     .then((response) => {
+//       console.log(response);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// };
+
+
+// const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+//   await rwClient.v2
+//     .tweet("Hello World!")
+//     .then((response) => {
+//       console.log(response);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//   });
+  
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({ ok: true }),
+//   };
+// };
+// main();
+// export { handler, main };
